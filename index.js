e1473ec66bcf67445e8f8a9e27aaec191510e491
Prathi Sahrudh <prathi.sahrudh@valuemomentum.com>
2025-11-21 14:37:13 +0530

    update: setup and call mongodb

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
- const express = require('express');
- const app = express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
- app.listen(3000, () => {
-    console.log('Server is running ' +  `http://localhost:3000`); 
- })
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://prathisahrudh_db_user:oegj4QzHVcKRd3ZW@backendapi.gxgylra.mongodb.net/?appName=BackendAPI"
+  )
+  .then(() => {
+      console.log("Connected to Database");
+      app.listen(3000, () => {
+        console.log("Server is running " + `http://localhost:3000`);
+      });
+  })
+  .catch((err) => {
+    console.log("Connection Failed");
+  });
